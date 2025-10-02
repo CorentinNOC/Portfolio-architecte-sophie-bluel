356d71dfc3bc5e1b84c759ccc71c9bd3ffd5a1a4
CorentinNOC <c.nicolas28210@gmail.com>
2025-10-02 15:48:57 +0200

    feat: display works

---
+++ FrontEnd/js/works.js
@@ -0,0 +1,30 @@
+let allWorks = [];
+
+export async function fetchWorks() {
+  try {
+    const response = await fetch("http://localhost:5678/api/works");
+    const data = await response.json();
+    allWorks = data;
+    return allWorks;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des travaux :", error);
+  }
+}
+
+// Display works
+export function displayWorks(currentWorks) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+
+  currentWorks.forEach((work) => {
+    const figure = document.createElement("figure");
+    figure.id = work.id;
+    figure.innerHTML = `
+      <img src="${work.imageUrl}" alt="${work.title}">
+      <figcaption>${work.title}</figcaption>
+    `;
+    gallery.appendChild(figure);
+  });
+}
+
+fetchWorks().then(displayWorks);
